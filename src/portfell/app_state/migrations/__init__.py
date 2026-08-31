3ac3fd0e6fc6292340575fcfae790c720964fd27
SSC <sergej.schweizer@gmail.com>
2026-08-31 23:40:17 +0200

    feat(pr384-analysis-job-progress-state): register analysis job migration

--- src/portfell/app_state/migrations/__init__.py
+++ src/portfell/app_state/migrations/__init__.py
@@ -1,5 +1,6 @@
 """Forward-only migration declarations for the clean app-state database."""
 
 from portfell.app_state.migrations.v001_initial import MIGRATION_V001
+from portfell.app_state.migrations.v002_analysis_jobs import MIGRATION_V002
 
-__all__ = ["MIGRATION_V001"]
+__all__ = ["MIGRATION_V001", "MIGRATION_V002"]
